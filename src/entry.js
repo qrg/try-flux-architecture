7fe6d71d348e15e8fdc805329ac0f6e8d0a874fd
qrg <qualle.rhythmus+d@gmail.com>
2016-09-16 12:32:16 +0900

    Delete sample files

--- src/entry.js
+++ src/entry.js
@@ -1,8 +1,1 @@
 'use strict';
-
-import React from 'react';
-
-import sample from './sample';
-
-console.log('this is entry.js');
-console.log(sample());
--- test/sample.test.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import test from 'ava';
-import sample from '../src/sample';
-
-test('returns string "sample"', t => {
-  t.is(sample(), 'sample');
-});
