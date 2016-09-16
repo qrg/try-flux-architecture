6aa703843b3ee82755329d0dc31a3419b4296b40
qrg <qualle.rhythmus+d@gmail.com>
2016-09-16 18:33:04 +0900

    Implement minimal Component

---
+++ src/component.js
@@ -0,0 +1,42 @@
+'use strict';
+
+import React from 'react';
+import ActionCreator from './action-creator';
+import Store from './store';
+import EventEmitter from './event-emitter';
+
+const dispatcher = new EventEmitter();
+const action = new ActionCreator(dispatcher);
+const store = new Store(dispatcher);
+
+export default class Component extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {count: store.getCount()};
+    this.tick = this.tick.bind(this);
+
+    // <- Observe store's change
+    store.on('CHANGE', () => {
+      this._onChange();
+    });
+  }
+
+  _onChange() {
+    this.setState({count: store.getCount()});
+  }
+
+  tick() {
+    action.countUp(this.state.count + 1);
+  }
+
+  render() {
+    return (
+      <div className='counter'>
+        <button onClick={this.tick} className='button is-outlined is-primary is-medium'>
+          Count Up
+        </button>
+        <span className='tag is-primary is-large'>{this.state.count}</span>
+      </div>
+    );
+  }
+}
