76969db9dbd533cf989e65952ecddb4d0bcfa708
qrg <qualle.rhythmus+d@gmail.com>
2016-09-16 20:00:04 +0900

    Render

--- src/entry.js
+++ src/entry.js
@@ -1,1 +1,10 @@
 'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Component from './component';
+
+ReactDOM.render(
+  <Component />,
+  document.getElementById('content')
+);
